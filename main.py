ede9558b8c3c6733b1df3019ccc97a947d313bef
RikoLi <939958083@qq.com>
2018-09-22 15:31:43 +0800

    2018.9.22

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from svmutil import *
 from func import *
 
 # Main
-label, sample = svm_read_problem('./data/formatted_training.txt')
-'''没进行归一化'''
-model = svm_train(label[1:len(label)-10], sample[1:len(sample)-10], '-s 2')
-p_label = svm_predict(label[10:], sample[10:], model)
-print(p_label)
+# -------------Start from here------------
+
 
